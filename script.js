d15c3f751cd5a8b9964d0cfc784bdc4ce741ec48
Ajay Chowdhury <ajaychowdhury1029@gmail.com>
2023-02-03 16:02:18 +0530

    added

--- script.js
+++ script.js
@@ -13,43 +13,49 @@ function init() {
 
 function play() {
   const copy = [...array];
-  const swaps = bubbleSort(copy);
-  animate(swaps);
+  const moves = bubbleSort(copy);
+  animate(moves);
 }
 
-function animate(swaps) {
-  if (swaps.length === 0) {
+function animate(moves) {
+  if (moves.length === 0) {
+    showBars();
     return;
   }
-  const [i, j] = swaps.shift();
-  [array[i], array[j]] = [array[j], array[i]];
-  showBars();
-  setTimeout(function(){
-    animate(swaps);
-  }, 50);
+  const move = moves.shift();
+  const [i, j] = move.indices;
+  if (move.type === "swap") [array[i], array[j]] = [array[j], array[i]];
+  showBars(move);
+  setTimeout(function () {
+    animate(moves);
+  }, 200);
 }
 
 function bubbleSort(array) {
+  const moves = [];
   do {
-    const swaps = [];
     var swapped = false;
     for (let i = 1; i < array.length; i++) {
+      moves.push({ indices: [i - 1, i], type: "comp" });
       if (array[i - 1] > array[i]) {
         swapped = true;
-        swaps.push([i - 1, i]);
+        moves.push({ indices: [i - 1, i], type: "swap" });
         [array[i - 1], array[i]] = [array[i], array[i - 1]];
       }
     }
   } while (swapped);
-  return swaps;
+  console.log("here");
+  return moves;
 }
 
-function showBars() {
+function showBars(move) {
   container.innerHTML = "";
   for (let i = 0; i < n; i++) {
     const bar = document.createElement("div");
     bar.style.height = array[i] * 100 + "%";
     bar.classList.add("bar");
+    if (move && move.indices.includes(i))
+      bar.style.backgroundColor = move.type === "swap" ? "red" : "blue";
     container.appendChild(bar);
   }
 }
